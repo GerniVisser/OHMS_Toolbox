886470aa5c0e885c494f703359cd476ef142b946
Gerni Visser <gerni.visser@gmail.com>
2022-07-12 10:06:52 +0200

    Moved Toolbox to its own view

--- Toolbox/MainWindow.xaml.cs
+++ Toolbox/MainWindow.xaml.cs
@@ -8,33 +8,10 @@ namespace Toolbox
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Views.ToolView _toolView;
         public MainWindow()
         {
-
             InitializeComponent();
-
-            setupTools();
         }
 
-        private void setupTools()
-        {
-            _toolView = new Views.ToolView();
-            ContentView.Children.Add(_toolView);
-
-            loadTools();
-        }
-
-        private void loadTools()
-        {
-            _toolView.AddTool("CaveabilityItem", new Caveability.Views.Caveability());
-            _toolView.AddTool("PillarStabilityItem", new PillarStability.Views.PillarStabiltyControl());
-        }
-
-        private void navigationDrawer_ItemClicked(object sender, Syncfusion.UI.Xaml.NavigationDrawer.NavigationItemClickedEventArgs e)
-        {
-            _toolView.ShowTool(e.Item.Name);
-            
-        }
     }
 }
---
+++ Toolbox/Views/ToolboxView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Toolbox.Views
+{
+    /// <summary>
+    /// Interaction logic for ToolboxView.xaml
+    /// </summary>
+    public partial class ToolboxView : UserControl
+    {
+        private Views.ToolView _toolView;
+        public ToolboxView()
+        {
+            InitializeComponent();
+
+            setupTools();
+        }
+
+        private void setupTools()
+        {
+            _toolView = new Views.ToolView();
+            ContentView.Children.Add(_toolView);
+
+            loadTools();
+        }
+
+        private void loadTools()
+        {
+            _toolView.AddTool("CaveabilityItem", new Caveability.Views.Caveability());
+            _toolView.AddTool("PillarStabilityItem", new PillarStability.Views.PillarStabiltyControl());
+        }
+
+        private void navigationDrawer_ItemClicked(object sender, Syncfusion.UI.Xaml.NavigationDrawer.NavigationItemClickedEventArgs e)
+        {
+            _toolView.ShowTool(e.Item.Name);
+
+        }
+    }
+}
