c242d1d742217d8ccaa15884c991168d7fc88e79
Gerni Visser <gerni.visser@gmail.com>
2023-05-02 17:42:22 +0200

    Borehole tool Init

---
+++ Borehole/ViewModels/BoreholeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using SharedWpfLibrary.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Borehole.ViewModels
+{
+    public class BoreholeViewModel : ViewModelBase
+    {
+
+    }
+}
